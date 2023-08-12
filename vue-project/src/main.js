b288c08326264ba739ac7968f4630eb3581ef170
Saki <iekongzixiao@163.com>
2023-08-12 19:24:33 +0800

    注释

--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -6,5 +6,5 @@ import './assets/main.css'
 new Vue({
         render: (h) => h(App)
     }).$mount('#app')
-    // 这是 Vue 项目的入口文件。在这里，你会初始化并挂载 Vue 实例到 DOM。你可能会看到如下的代码：
+    // 这是 Vue 项目的入口文件。在这里，你会初始化并挂载 Vue 实例到 DOM。
     // 这段代码会将 App.vue 组件挂载到 HTML 中的 #app 元素上。
