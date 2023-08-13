5c3781a9efb89af88cf1dd1c0f8f3ee515f3379b
Saki <iekongzixiao@163.com>
2023-08-13 14:25:13 +0800

    save

--- vue-project/src/components/hello.vue
+++ vue-project/src/components/hello.vue
@@ -1,44 +1,26 @@
-<script setup>
-defineProps({
-  msg: {
-    type: String,
-    required: true
-  }
-})
-</script>
-
 <template>
-  <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
-    <h3>
-      You’ve successfully created a project with
-      <a target="_blank" href="https://vitejs.dev/">Vite</a> +
-      <a target="_blank" href="https://v2.vuejs.org/">Vue 2</a>.
-    </h3>
-  </div>
+    <div class="container absolute flex items-center justify-center">
+        <div class="text-5xl font-medium w-full text-center">
+        Hello World!
+        </div>
+        <button @click="back" class="absolute top-20 flex-none border-2 rounded-full pl-10 pr-10 pt-3 pb-3 shadow bg-gradient-to-t from-p1_cbutton1 to-p1_cbutton2 text-white text-xl font-light">
+            back
+        </button>
+    </div>
+    
 </template>
 
-<style scoped>
-h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
-  top: -10px;
-}
+<script>
+export default{
+    data(){
+        return{
 
-h3 {
-  font-size: 1.2rem;
+        }
+    },
+    methods:{
+        back(){
+            this.$router.replace('/new_homepage')
+        }
+    }
 }
-
-.greetings h1,
-.greetings h3 {
-  text-align: center;
-}
-
-@media (min-width: 1024px) {
-  .greetings h1,
-  .greetings h3 {
-    display: block;
-    text-align: left;
-  }
-}
-</style>
+</script>
--- vue-project/src/components/main.vue
+++ vue-project/src/components/main.vue
@@ -1,86 +1,43 @@
 <template>
-  <div class="item">
-    <i>
-      <slot name="icon"></slot>
-    </i>
-    <div class="details">
-      <h3>
-        <slot name="heading"></slot>
-      </h3>
-      <slot></slot>
-    </div>
-  </div>
+	<body class="font-Sara items-center overflow-x-hidden">
+		<part1/>
+		<part2/>
+		<part3/>
+		<part4/>
+	</body>	
 </template>
 
 <style scoped>
-.item {
-  margin-top: 2rem;
-  display: flex;
-}
 
-.details {
-  flex: 1;
-  margin-left: 1rem;
-}
-
-i {
-  display: flex;
-  place-items: center;
-  place-content: center;
-  width: 32px;
-  height: 32px;
-
-  color: var(--color-text);
-}
-
-h3 {
-  font-size: 1.2rem;
-  font-weight: 500;
-  margin-bottom: 0.4rem;
-  color: var(--color-heading);
-}
-
-@media (min-width: 1024px) {
-  .item {
-    margin-top: 0;
-    padding: 0.4rem 0 1rem calc(var(--section-gap) / 2);
-  }
-
-  i {
-    top: calc(50% - 25px);
-    left: -26px;
-    position: absolute;
-    border: 1px solid var(--color-border);
-    background: var(--color-background);
-    border-radius: 8px;
-    width: 50px;
-    height: 50px;
-  }
-
-  .item:before {
-    content: ' ';
-    border-left: 1px solid var(--color-border);
-    position: absolute;
-    left: 0;
-    bottom: calc(50% + 25px);
-    height: calc(50% - 25px);
-  }
-
-  .item:after {
-    content: ' ';
-    border-left: 1px solid var(--color-border);
-    position: absolute;
-    left: 0;
-    top: calc(50% + 25px);
-    height: calc(50% - 25px);
-  }
-
-  .item:first-of-type:before {
-    display: none;
-  }
-
-  .item:last-of-type:after {
-    display: none;
-  }
-}
 </style>
+
+<script>
+	import part1 from './all_part/part1.vue'
+	import part2 from './all_part/part2.vue'
+	import part3 from './all_part/part3.vue'
+	import part4 from './all_part/part4.vue'
+	export default{
+		data() {
+			return {}
+		},
+		methods:{},
+		created(){
+		},
+		components:{part1,part2,part3,part4},
+	}
+	//导航栏响应滚动变色
+	window.onscroll=function(){
+		var topScroll =window.pageYOffset;
+		console.log(topScroll)
+		var nav=document.getElementById("nav");
+		if(topScroll>120){
+			nav.style.backgroundColor = 'rgba(255,255,255,0.75)';
+			nav.style.backdropFilter = 'blur(24px)';
+		}
+		else{
+			nav.style.backgroundColor = '';
+			nav.style.backdropFilter = '';
+		}
+	}
+	
+</script>
